a2d7937a5f298d0b2aa589dd1aeb453446d307a5
lucasrochaabraao <lucasrabraao@gmail.com>
2020-10-07 14:54:59 -0300

    adding sample config files

---
+++ config_sample.py
@@ -0,0 +1,10 @@
+# Mantenho nesse arquivo os Emails para enviar as mensagens de saldo, e a chave API para consultar o gupshup.
+
+class Email:
+    email_alpha = ("email.alpha@domain.com",)
+    email_bravo = ("email.bravo@domain.com", "mypsswrd@domain")
+    email_charlie = ("email.charlie@gdomain.com", "random@pssw")
+
+class GupshupSaldo:
+    apikey = '1234567890abcdef'
+
